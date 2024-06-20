420044015be41c70636923925e62d3436623c13c
Prassu <Prassu@gmail.com>
2024-06-20 22:04:07 +0530

    all files commited from pipeline maven

---
+++ src/main/java/com/example/pipeline_maven/PipelineMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineMavenApplication.class, args);
+	}
+
+}
